63988ed8374874fcb5fa07e3b506c2424acf485c
benlauring7 <55059907+benlauring7@users.noreply.github.com>
2021-12-13 16:35:23 -0500

    Update santorini.py

--- santorini.py
+++ santorini.py
@@ -13,9 +13,9 @@ class Square():
 
     def __str__(self):
         if (self.occupant == None):
-            return '{} '.format(self.level)
+            return '|{} '.format(self.level)
         else:
-            return '{}{}'.format(self.level, self.occupant)
+            return '|{}{}'.format(self.level, self.occupant)
 
 
 class Board():
@@ -24,19 +24,25 @@ class Board():
         self.memento = Memento()
 
     def create_board(self):
+        '''create 2d array board w/o borders'''
         board = []
         for row in range(5):
             new_row = []
             for col in range(5):
                 new_row.append(Square(row, col))
+                board.append(new_row)
+        return board
 
     def print_board(self):
-        while True:
+        i = 0
+        j = 0
+        while i < 5:
             print('+--+--+--+--+--+')
-            if 
-            for square in self.squares:
+            i+=1
+            while j < 5:
+                self.squares[i][j]
+                j+=1
+            if j == 5:
                 print('|')
-                print(square)
-
-
-
+        print('+--+--+--+--+--+')
+        

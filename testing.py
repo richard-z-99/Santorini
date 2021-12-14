06312ac06e8eca4312407efd324016e4b482bf5f
richard-z-99 <78984520+richard-z-99@users.noreply.github.com>
2021-12-14 16:50:49 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,40 @@
-from main import PlayGame
-from santorini_classes import Move
+from main import PlayGame, Memento
+from santorini_classes import Move, Build, Worker, Board
 
 game = PlayGame()
-game.board.print_board()
+board = game.memento.history[0]
+a = board.white_player.worker1
+#print("{}, {}".format(a.row, a.col))
+move1 = Move('n', a)
+build1 = Build('n', a)
+build2 = Build('w', a)
+
+print("----------Starting----------")
+game.print_curr_board()
+game.execute_move(move1)
+
+print("move 1:")
+game.print_curr_board()
+
+if(game.check_build(build1)):
+    game.execute_build(build1)
+else:
+    print("boo!")
+    game.execute_build(build2)
+
+print("build 1:")
+game.print_curr_board()
+
+game.undo()
+print("after undo:")
+game.print_curr_board()
+
+game.redo()
+print("after redo:")
+game.print_curr_board()
+
+
+
+
+
+

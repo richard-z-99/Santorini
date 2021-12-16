40c61b3cbb838b130ee3e5887f451495c22e4cb9
benlauring7 <55059907+benlauring7@users.noreply.github.com>
2021-12-15 19:08:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from santorini_classes import Action, Build, HumanPlayer, RandomPlayer, Move
+from santorini_classes import*
 from board import Board
 import copy
 import sys
@@ -39,38 +39,49 @@ class Memento:
         self.history.append(new_board)
         self.cur_board += 1
 
+    def prompt(self):
+        options = input("undo, redo, or next\n")
+        current_board = self.history[self.cur_board]
+        while options != "next":
+            current_board.print_board()
+            if options == "undo":
+                self.undo()
+            if options == "redo":
+                self.redo()
+            print("Turn: {}, {} ({}{})".format(self.cur_board+1, current_board.curr_player.color, current_board.curr_player.worker1.name, current_board.curr_player.worker2.name))
+            options = input("undo, redo, or next\n")
 
 
-class RunHuman:
-    #outputs instructions for human player, reads input, and executes corresponding move
-    def run(self, game):
-        player = game.board.curr_player
+# class RunHuman:
+#     #outputs instructions for human player, reads input, and executes corresponding move
+#     def run(self, game):
+#         player = game.board.curr_player
 
-        #TODO: loop until valid name inputted
-
-
-        worker_name = input("Select a worker to move\n>")
-        if(worker_name == player.worker1.name): 
-            worker = player.worker1
-
-        elif(worker_name == player.worker2.name):
-            worker = player.worker2
+#         #TODO: loop until valid name inputted
 
 
-        #TODO: Error checking on move_direction, actually moving worker. DONE, i think
+#         worker_name = input("Select a worker to move\n>")
+#         if(worker_name == player.worker1.name): 
+#             worker = player.worker1
+
+#         elif(worker_name == player.worker2.name):
+#             worker = player.worker2
+
+
+#         #TODO: Error checking on move_direction, actually moving worker. DONE, i think
         
-        move_dir = input("Select a direction to move {}\n".format(directions.keys()))
-        move = Move(move_dir, worker)
-        if(game.check_move(move)):
-            game.execute_move(move)
+#         move_dir = input("Select a direction to move {}\n".format(directions.keys()))
+#         move = Move(move_dir, worker)
+#         if(game.check_move(move)):
+#             game.execute_move(move)
 
 
-        #TODO: implement build. DONE, i think
-        build_dir = input("Select a direction to build {}\n".format(directions.keys()))
-        build = Build(build_dir, worker)
-        #no other checks on building besides check_board.
-        if (game.check_build(build)):
-            game.execute_build(build)
+#         #TODO: implement build. DONE, i think
+#         build_dir = input("Select a direction to build {}\n".format(directions.keys()))
+#         build = Build(build_dir, worker)
+#         #no other checks on building besides check_board.
+#         if (game.check_build(build)):
+#             game.execute_build(build)
 
 
 
@@ -99,10 +110,22 @@ class PlayGame:
 
     
     def run(self):
+        self.print_curr_board()
         #change while condition to not_win later
-        while self.board.check_won(self.board.curr_player) is False:
+        while True:
+            print("Turn: {}, {} ({}{})".format(self.memento.cur_board+1, self.board.curr_player.color, self.board.curr_player.worker1.name, self.board.curr_player.worker2.name))
+            
+            if not (self.board.check_won(self.board.curr_player) is None and self.board.check_won(self.board.get_opponent(self.board.curr_player)) is None):
+                break
+            
+            if sys.argv[3] == "on":
+                self.memento.prompt()
+
+            # if sys.argv[4] == "on":
+            #     pass
+
             if isinstance(self.board.curr_player, HumanPlayer):
-                self.print_curr_board()
+                # print("Turn: {}, {} ({}{})".format(self.memento.cur_board+1, self.board.curr_player.color, self.board.curr_player.worker1.name, self.board.curr_player.worker2.name))
                 curr_worker = self.board.curr_player.choose_worker()
                 legal_moves = self.board.get_legal_moves(self.board.curr_player)
                 move = self.board.curr_player.choose_move(legal_moves, curr_worker)
@@ -110,21 +133,50 @@ class PlayGame:
                 legal_builds = self.board.get_legal_builds(self.board.curr_player, move.worker)
                 build = self.board.curr_player.choose_build(legal_builds, curr_worker)
                 self.execute_build(build)
-                self.board.switch_player()
+                self.print_curr_board()
+                
 
         #if bot
-            else:
-                self.print_curr_board()
+            elif isinstance(self.board.curr_player, RandomPlayer):
+                # print("Turn: {}, {} ({}{})".format(self.memento.cur_board+1, self.board.curr_player.color, self.board.curr_player.worker1.name, self.board.curr_player.worker2.name))
                 legal_moves = self.board.get_legal_moves(self.board.curr_player)
                 move = self.board.curr_player.choose_move(legal_moves)
+                # print(move.worker.name)
+                # print(move.direction)
                 self.execute_move(move)
                 legal_builds = self.board.get_legal_builds(self.board.curr_player, move.worker)
                 build = self.board.curr_player.choose_build(legal_builds)
+                # print(build.worker.name)
+                # print(build.direction)
                 self.execute_build(build)
-                self.board.switch_player()
-                print("NEW TURN")
+                print("{},{},{}".format(move.worker.name, move.direction, build.direction))
+                self.print_curr_board()
+                #self.board.switch_player()
+                # print("NEW TURN")
             #bot choose move will not have curr worker. also choose_build. Curr_worker is just a property of the legal_move that was chosen by the algo
         #change curr_player, iterate turn =+1. Can implement in execute_build
+
+            elif isinstance(self.board.curr_player, HeuristicPlayer):
+                legal_moves = self.board.get_legal_moves(self.board.curr_player)
+                move = self.board.get_best_move(legal_moves)
+                self.execute_move(move)
+
+                # print(move.direction)
+                # print(move.worker.name)
+
+                legal_builds = self.board.get_legal_builds(self.board.curr_player, move.worker)
+                build = self.board.get_best_build(legal_builds)
+                self.execute_build(build)
+                print("{},{},{}".format(move.worker.name, move.direction, build.direction))
+                self.print_curr_board()
+                #self.board.switch_player()
+                # print("NEW TURN")
+
+            else:
+                print("ERROR: Invalid player type!")
+
+            self.board.switch_player()
+            # print(self.board.curr_player.color)
 
 
     def execute_move(self, move):
@@ -135,33 +187,9 @@ class PlayGame:
 
         self.board.execute_move(move)
 
-        # #update location of worker
-        # old_row = move.worker.row
-        # old_col = move.worker.col
-        # new_row = move.get_new_coords()[0]
-        # new_col = move.get_new_coords()[1]
-
-        # move.worker.update_location(new_row, new_col)
-
-        # #update occupancy status of old/new squares
-        # old_square = self.board.get_square(old_row, old_col)
-        # old_square.update_occupant(None)
-
-        # new_square = self.board.get_square(new_row, new_col)
-        # new_square.update_occupant(move.worker)
-
-
 
 
     def execute_build(self, build):
-        # new_row = build.get_new_coords()[0]
-        # new_col = build.get_new_coords()[1]
-
-        # #update level of new_square
-        # #in main, need to check if valid build (not building above level 4)
-        # new_square = self.board.get_square(new_row, new_col)
-        # new_square.update_level()
-
         self.board.execute_build(build)
 
         #update memento
@@ -176,6 +204,16 @@ class PlayGame:
 if __name__ == "__main__":
     kind1 = sys.argv[1]
     kind2 = sys.argv[2]
+
+    # memory = sys.argv[3]
+    # score_display = sys.argv[4]
+
+    # if sys.argv[3] == "on":
+    #     pass
+    #     #turn on undo/redo/next functionality
+    # if sys.argv[4] == "on":
+    #     pass
+        #turn on score display
 
     game1 = PlayGame(kind1, kind2)
     game1.run()
